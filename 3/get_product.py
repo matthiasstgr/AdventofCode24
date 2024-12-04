a629f0b2b81b0597ba249d04466bf43a151271b0
matthias.steger <matthias.stgr@gmail.com>
2024-12-04 09:46:08 +0100

    Did Task 2 Day 2!

--- 3/get_product.py
+++ 3/get_product.py
@@ -8,8 +8,22 @@ def calculate_product(data) -> int:
         product += int(nums[0]) * int(nums[1])
     return product
 
+def get_filtered_data(data) -> str:
+    filtered_data = data
+    while True:
+        old_filtered_data = filtered_data
+        filtered_data = re.sub(r'don\'t\(\)(.|\n)*?do\(\)', '', filtered_data, re.DOTALL)
+        if filtered_data == old_filtered_data:
+            break
+    return(filtered_data)
+
 text = input.read()
 correct_data = re.findall(r'mul\(\d+,\d+\)', text)
 product = calculate_product(correct_data)
-print(product)
+print('Without do and dont:', product)
+
+filtered_text = get_filtered_data(text)
+correct_filtered_data = re.findall(r'mul\(\d+,\d+\)', filtered_text)
+filtered_product = calculate_product(correct_filtered_data)
+print('With do and dont:', filtered_product)
 
